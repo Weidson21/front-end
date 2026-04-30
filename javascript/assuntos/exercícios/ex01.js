481564ce8cbe0869986422f339f7431ede1b88c5
weidson <weidsoniarley86@gmail.com>
2026-04-30 01:05:06 -0300

    feat: variáveis js.

---
+++ javascript/assuntos/exercícios/ex01.js
@@ -0,0 +1,3 @@
+let fiatUno = {modelo: "Fiat Uno", ano: 1989, cores: ["vermelho", "branco", "preto"]};
+
+fiatUno.cores;
---
+++ javascript/assuntos/javascript.js
@@ -0,0 +1,31 @@
+// recurso de comentários
+let fruta = "maça";
+
+// tipos de dados em variáveis
+
+// string
+let nome = "Weidson";
+
+// Integers
+
+let idade = 24;
+
+// floats
+
+let contaBancaria = 765.80;
+
+// booleans
+// true
+// false
+
+
+// arrays
+
+let frutas = ["maça", "pera", "banana", "laranja"];
+// acessando item específico no array
+frutas[0];
+
+// objetos
+
+let xicara = {cor: "branco"};
+xicara.cor
